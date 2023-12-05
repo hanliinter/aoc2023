230b2254fa93f3cbd43f08baf4c389b2a0446740
Hanli Wang <hanliinter@gmail.com>
2023-12-05 18:17:09 +0100

    Part 1

---
+++ Day5/main.hs
@@ -0,0 +1,50 @@
+module Main where
+
+import Data.List
+import Data.Foldable
+import Data.Functor
+import Data.Char
+wordsWhile :: (a->Bool) -> [a] -> [[a]]
+wordsWhile p xs = case dropWhile p xs of
+                    [] -> []
+                    xs' -> w: wordsWhile p xs''
+                     where (w,xs'') = break p xs'
+
+
+
+trim :: String -> String
+trim = f . f
+   where f = reverse . dropWhile isSpace
+
+
+main :: IO ()
+main = readFile "input.txt" <&> lines <&> solvePart1 <&> show >>= putStrLn
+
+
+
+solvePart1 :: [String] -> [Int]
+solvePart1 contents = let (seedsString:s2s:s2f:f2w:w2l:l2t:t2h:_) = wordsWhile (== "") contents
+                          seeds = map read $ words $ last $ wordsWhile (== ':') $ head seedsString
+                          rangesStrings = tail $ wordsWhile (== "") contents
+                          ranges = map go $ map tail rangesStrings
+                      in
+                        sort $ map (\x -> foldl' convert x ranges ) seeds
+                        
+                          
+                            where
+                              go str = map go' str
+                                where go' line = let (a:b:c:_) = map read $ words line
+                                                 in
+                                                   --zip [b..(b+c-1)] [a..]
+                                                   (a,b,c)
+                          
+
+convert :: Int -> [(Int,Int,Int)] -> Int
+convert s dict = case asum $ map (go s) dict of
+                   Just c -> c
+                   Nothing -> s
+  where
+    go :: Int -> (Int,Int,Int) -> Maybe Int
+    go s (a,b,c) = if (s - b) >= 0 && (s -b) < c then Just (a + (s - b))
+                                                    else Nothing
+
