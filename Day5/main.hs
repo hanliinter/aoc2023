e7456fac14614f701cf4dca1ce0cac47d54e2bb5
Hanli Wang <hanliinter@gmail.com>
2023-12-06 20:45:26 +0100

    Use Parallel is enough

--- Day5/main.hs
+++ Day5/main.hs
@@ -4,6 +4,9 @@ import Data.List
 import Data.Foldable
 import Data.Functor
 import Data.Char
+import Control.DeepSeq
+import Control.Parallel.Strategies
+import System.Environment
 wordsWhile :: (a->Bool) -> [a] -> [[a]]
 wordsWhile p xs = case dropWhile p xs of
                     [] -> []
@@ -18,18 +21,17 @@ trim = f . f
 
 
 main :: IO ()
-main = readFile "input.txt" <&> lines <&> solvePart1 <&> show >>= putStrLn
-
-
-
-solvePart1 :: [String] -> [Int]
+main = getArgs <&> head  >>= readFile  <&> lines <&> solvePart1 <&> show >>= putStrLn
+solvePart1 :: [String] -> Int
 solvePart1 contents = let (seedsString:s2s:s2f:f2w:w2l:l2t:t2h:_) = wordsWhile (== "") contents
                           seeds = map read $ words $ last $ wordsWhile (== ':') $ head seedsString
+                          seedsList = seedsInPart2  seeds
                           rangesStrings = tail $ wordsWhile (== "") contents
                           ranges = map go $ map tail rangesStrings
+                          result = map  (map (\x -> foldl convert x ranges )) seedsList
+                          result' = result  `using` parBuffer 10000 rseq
                       in
-                        sort $ map (\x -> foldl' convert x ranges ) seeds
-                        
+                        minimum $ map minimum result'
                           
                             where
                               go str = map go' str
@@ -48,3 +50,11 @@ convert s dict = case asum $ map (go s) dict of
     go s (a,b,c) = if (s - b) >= 0 && (s -b) < c then Just (a + (s - b))
                                                     else Nothing
 
+
+
+seedsInPart2 :: [Int] -> [[Int]]
+seedsInPart2 xs = if length xs `mod` 2 /= 0 then error "reading list error"
+                                            else go xs []
+                      where go :: [Int] -> [[Int]] -> [[Int]]
+                            go [] result = result
+                            go (x:y:rest) result = go rest ([x..(x+y-1)]:result) 
