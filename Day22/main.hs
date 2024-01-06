cf470878be48852c4d03e762dbf2918e5230ebc7
Hanli Wang <hanliinter@gmail.com>
2024-01-06 02:00:06 +0100

    Debug Trace save the day

--- Day22/main.hs
+++ Day22/main.hs
@@ -1,12 +1,20 @@
 module Main where
-
+import Control.Monad
 import Data.Functor
 import Data.Function
 import Data.List
+import Debug.Trace
 import qualified Data.Map as Map
 
 main :: IO ()
 main = readFile "input.txt" <&> lines <&> solvePart1 <&> show >>= putStrLn
+
+-- main :: IO ()
+-- main = do
+--   str <- readFile "input.txt" <&> lines <&> solvePart1
+--   putStrLn $ str
+--   return ()
+
 type Endpoint = (Int,Int,Int)
 
 
@@ -15,10 +23,32 @@ type SMap = Map.Map (Int,Brick) [Brick]
 
 data Brick = Brick Endpoint Endpoint deriving (Show, Eq)
 
+showBrick :: Brick -> String
+showBrick (Brick (x1,y1,z1) (x2,y2,z2)) = let first = concat $ intersperse "," (map show [x1,y1,z1])
+                                              second = concat $ intersperse "," (map show [x2,y2,z2])
+                                          in
+                                            first ++ "~" ++ second
+
+
+getLowX (Brick (x1,_,_) (x2,_,_)) = min x1 x2
+getLowY (Brick (_,y1,_) (_,y2,_)) = min y1 y2
+getLowZ (Brick (_,_,z1) (_,_,z2)) = min z1 z2
+getHighX (Brick (x1,_,_) (x2,_,_)) = max x1 x2
+getHighY (Brick (_,y1,_) (_,y2,_)) = max y1 y2
+getHighZ (Brick (_,_,z1) (_,_,z2)) = max z1 z2
+
 
 instance Ord Brick where
   (<=) :: Brick -> Brick -> Bool
-  (<=) (Brick _ (_,_,z1)) (Brick _ (_,_,z2)) = z1 <= z2
+  (<=) b1 b2 = let x1 = getLowX b1
+                   x2 = getLowX b2
+                   y1 = getLowY b1
+                   y2 = getLowY b2
+                   z1 = getLowZ b1
+                   z2 = getLowZ b2
+                   in
+               (z1,x1,y1) <= (z2,x2,y2)
+                   
 
 readBrick :: String -> Brick
 readBrick str = let firstEnd = map read $ wordsWhile (==',') $ head $ wordsWhile (=='~') str
@@ -41,26 +71,30 @@ overlap (x1s,x1e) (x2s,x2e) (y1s,y1e) (y2s,y2e) = (max x1s x2s) <= (min x1e x2e)
 isSupporting :: Brick -> Brick -> Bool
 isSupporting brickA brickB = overlapBrick brickA brickB && getHighZ brickA +1 == getLowZ brickB
 
-getLowZ (Brick (_,_,z1) (_,_,z2)) = min z1 z2
-getHighZ (Brick (_,_,z1) (_,_,z2)) = max z1 z2
 
+testGetBrickXY (Brick (x1,y1,_) (x2,y2,_)) = (x1,y1,x2,y2)
 fallDown :: [Brick] -> [Brick]
 fallDown bricks = let  
-                       comp = compare `on` getLowZ
-                       sorted = sortBy comp bricks
+                       sorted = sort bricks
                     in
-                    
-                     foldl go [] sorted
+                    --sorted
+                    sortBy (compare `on` getHighZ) $ foldl' go [] sorted
       where 
             go :: [Brick] -> Brick -> [Brick]
-            go [] current = [downTo 1 current]
             go stable current  = let l = filter (overlapBrick current) stable
-                                          in
+                                  in
+                                   -- if current == Brick (4,8,7) (5,8,7) then traceShow l $
+                                   --  case l of
+                                   --            [] -> insert (downTo 1 current) stable 
+                                   --            stable' -> let h = maximum $ map getHighZ stable'
+                                   --                       in
+                                   --                        traceShowId (insert (downTo (h +1) current) stable)
+                                   -- else
                                             case l of
                                               [] -> insert (downTo 1 current) stable
-                                              stable' -> let (h:_) = reverse stable'
+                                              stable' -> let h = maximum $ map getHighZ stable'
                                                              in
-                                                           insert (downTo ((getHighZ h) +1) current) stable
+                                                           insert (downTo (h+1) current) stable
             insert :: Brick -> [Brick] -> [Brick]
             insert x [] = [x]
             insert x (y:ys) = if x <=y
@@ -74,15 +108,16 @@ markBrick bricks = Map.fromList $ zip bricks [0..]
             
 findSupportingBelow :: [Brick] -> (Map.Map Brick [Brick],Map.Map Brick [Brick])
 findSupportingBelow bricks = let support = Map.fromList $ map (\x -> (x,[])) bricks in
-                                   go [] bricks (Map.empty,Map.empty)
+                                   go [] bricks (Map.empty,support)
   where go prev [] (dict,support) = (dict,support)
         go prev (a:rest) (dict,support)  = let --(i,a') = a
                                      cs = filter (`isSupporting` a) prev
                                      dict' = Map.insert a cs dict
                                      support' = foldl' (prepare a) support cs
+                                     prepare a support c = Map.insertWith (++) c [a] support
                                   in
                                    go (a:prev) rest (dict', support')
-        prepare a support c = Map.insertWith (++) c [a] support
+        
 
 
 wordsWhile :: (a->Bool) -> [a] -> [[a]]
@@ -101,24 +136,22 @@ isSingleAxisChange str = let firstEnd = map read $ wordsWhile (==',') $ head $ w
 
 
 solvePart1 contents = let bricks =  map (readBrick) contents
-                          
-                          comp = (flip compare) `on` getLowZ
                           (supportBy, support) =  findSupportingBelow $ fallDown bricks
-                          
                       in
-                        --markBrick $ fallDown bricks
+--                        map showBrick $ fallDown bricks
+                      --  fallDown bricks
                        --supportBy
                       -- support
-                       length $ nub $ sort $ filter (\b -> go b supportBy support) $ fallDown bricks
+                     length $ filter (\b -> go b supportBy support) $ fallDown bricks
                        
                        
                        -- lookup brick  (Map.toList supportBy)
-    where go b supportBy support = case lookup b (Map.toList $ support) of
-                                     Nothing -> True -- error $ "should never happen" ++ show b ++ (show $ Map.toList support)
+    where go b supportBy support = case Map.lookup b  support of
+                                     Nothing -> error $ "should never happen" ++ show b ++ (show $ Map.toList support)
                                      Just c -> case c of
-                                       [] -> True
+                                       [] -> True --error $ show b ++ "test"
                                        xs -> all (\b' -> go' b' supportBy) xs
-          go' b supportBy  = case lookup b (Map.toList supportBy) of
+          go' b supportBy  = case Map.lookup b supportBy of
                                       Nothing -> error $ "Should never happen" ++ show b ++ "\n" ++ show supportBy
                                       Just c -> case c of
                                         [] -> error "also should never happen"
